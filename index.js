9c608e7bc4c464038dcb35d2a7688dbb2e15aa33
Ravi <ravivyas922@gmail.com>
2023-01-24 00:03:34 -0500

    Weight Calculator Working

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 
-const calculator = document.getElementById("Calculator");
-calculator.addEventListener('submit',function(e)
+
+const form = document.getElementById('Calculator');
+const currentWeight = document.getElementById("currentWeight");
+const targetWeight = document.getElementById("targetWeight");
+const weightDays = document.getElementById("weightDays");
+form.addEventListener("submit",function(e)
 {
     e.preventDefault();
-    let currentWeight = document.getElementById("currentWeight");
-    console.log(currentWeight);
-}); 
+    const currentWeightValue = currentWeight.value;
+    const targetWeightValue = targetWeight.value;
+    const weightDaysValue = weightDays.value;
+    let weightOutput;
+    let weightDifference = currentWeightValue - targetWeightValue;
+    if(weightDifference < 0)
+    {
+        weightOutput = weightDifference*-1/weightDaysValue;
+        console.log(weightOutput);
+    }
+    else
+    {
+        weightOutput = weightDifference/weightDaysValue;
+        console.log(weightOutput);
+    }
+    
+});
 
 
