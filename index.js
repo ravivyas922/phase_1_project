618c24ba92a8359448d61ba2019c2210b819a17e
Ravi <ravivyas922@gmail.com>
2023-01-23 23:27:13 -0500

    Second Test Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+const calculator = document.getElementById("Calculator");
+calculator.addEventListener('submit',function(e)
+{
+    e.preventDefault();
+    let currentWeight = document.getElementById("currentWeight");
+    console.log(currentWeight);
+}); 
+
+
